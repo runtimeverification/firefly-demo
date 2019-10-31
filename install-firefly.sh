c214d86a329b3c987ac2af87e89763731807bb8b
ehildenb <everett.hildenbrandt@gmail.com>
2019-10-30 23:43:23 -0600

    Initialize CI setup (#1)
    * soldity/hello-world: example program
    
    * .gitignore: ignore generate evm
    
    * Makefile: recipe for compiling Solidity code
    
    * Dockerfile, Jenkinsfile: initial testing setup
    
    * {. => jenkins}/{Dockerfile,Jenkinsfile}: subdirectory for jenkins specific stuff
    
    * jenkins/Jenkinsfile: correct path
    
    * jenkins/Jenkinsfile: put dockerfile in agent block
    
    * jenkins/Jenkinsfile: agent none at toplevel
    
    * jenkins/Dockerfile: no need for sudo
    
    * jenkins/{Jenkinsfile,Dockerfile}: use user:user account
    
    * jenkins/Dockerfile: add add-apt-repository command
    
    * install-firefly.sh: install script for firefly
    
    * jenkinsfile/Dockerfile: correct package for add-apt-repository
    
    * jenkins/Dockerfile: minimize dependencies
    
    * jenkinsfile/Dockerfile: add --yes to apt-get install

---
+++ install-firefly.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -ueo pipefail
+shopt -s extglob
+
+notif() {
+    echo "== $0: $@" 2>&1
+}
+
+fatal() {
+    notif "$@"
+    exit 1
+}
+
+distro="$1" ; shift
+
+download_url_base='https://github.com/kframework/evm-semantics/releases/download'
+download_version='v1.0.0-3cecbb6'
+
+case "$distro" in
+    ubuntu-bionic ) download_pkg_name='kevm_1.0.0_amd64_bionic.deb' ;;
+    debian-buster ) download_pkg_name='kevm_1.0.0_amd64_buster.deb' ;;
+    *             ) fatal "Unknown distro: $distro"
+esac
+
+download_url="$download_url_base/$download_version/$download_pkg_name"
+curl --output "$download_pkg_name" "$download_url"
+
+case "$distro" in
+    @(ubuntu-bionic|debian-buster) ) sudo apt-get install "$download_pkg_name"
+esac
