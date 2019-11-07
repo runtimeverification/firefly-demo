ccafad1c5e9a0f67ce1f7b8a03a6a154a426c1e2
ehildenb <everett.hildenbrandt@gmail.com>
2019-11-06 23:33:22 -0600

    Run firefly tests on CI (#3)
    * run-firefly.sh: actual runner setup
    
    * jenkins/Jenkinsfile: actually run the firefly test
    
    * run-firefly: correct directory for temporary files
    
    * jenkins/Dockerfile: add git dependency
    
    * jenkins/Dockerfile: install npm/npx
    
    * jenkins/Dockerfile: install gcc
    
    * jenkins/Dockerfile: install g++
    
    * jenkins/Jenkinsfile: add timeout to firefly tests
    
    * jenkins/Dockerfile: add netcat-openbsd dependency
    
    * jenkins/Dockerfile: install protobuf compiler
    
    * install-firefly: missing ;;

--- install-firefly.sh
+++ install-firefly.sh
@@ -20,7 +20,7 @@ download_version='v1.0.0-3cecbb6'
 case "$distro" in
     ubuntu-bionic ) download_pkg_name='kevm_1.0.0_amd64_bionic.deb' ;;
     debian-buster ) download_pkg_name='kevm_1.0.0_amd64_buster.deb' ;;
-    *             ) fatal "Unknown distro: $distro"
+    *             ) fatal "Unknown distro: $distro"                 ;;
 esac
 
 download_url="$download_url_base/$download_version/$download_pkg_name"
