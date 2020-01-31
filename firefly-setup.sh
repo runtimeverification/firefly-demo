0525da7775f49cdbce7855f01b3e75669f0d07dc
Everett Hildenbrandt <everett.hildenbrandt@gmail.com>
2020-01-31 21:28:48 +0000

    firefly-setup: use new K binary

--- firefly-setup.sh
+++ firefly-setup.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 # export FIREFLY_TOKEN="U052ZEQvZjVpZ0lQbjd1R1NlNFJpZGNTbVppbVFFSng4RWhOclpaTVk2RT0="
 # curl https://sandbox.fireflyblockchain.com/script?firefly_token="$FIREFLY_TOKEN" | sh
 
-curl --location --output k.tar.gz 'https://github.com/kframework/k/releases/download/v5.0.0-9985955/k-nightly.tar.gz'
+curl --location --output k.tar.gz 'https://github.com/kframework/k/releases/download/v5.0.0-280480e/k-nightly.tar.gz'
 tar --verbose --extract --file k.tar.gz
 export K_RELEASE=$(pwd)/k
 
