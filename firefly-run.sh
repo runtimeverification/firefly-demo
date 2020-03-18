bdfeef8693e0fa88a99e1a529e6953ad18f5fb60
Guy Repta <50716988+gtrepta@users.noreply.github.com>
2020-03-18 18:43:37 -0500

    tests/HelloWorld: Add simple truffle test

--- firefly-run.sh
+++ firefly-run.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 export FIREFLY_TOKEN="U052ZEQvZjVpZ0lQbjd1R1NlNFJpZGNTbVppbVFFSng4RWhOclpaTVk2RT0="
 
 cd firefly
-./firefly prep tests/tools/random-testing/coverage
-./firefly launch tests/tools/random-testing/coverage 8540
-./firefly test tests/tools/random-testing/coverage 8540
+./firefly prep ../tests/HelloWorld
+./firefly launch ../tests/HelloWorld 8145
+./firefly test ../tests/HelloWorld 8145
