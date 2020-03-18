d9215cfca12ec1aa9016245eb7e6f9becb103f2c
Guy Repta <50716988+gtrepta@users.noreply.github.com>
2020-03-18 17:31:23 -0500

    firefly-setup.sh: Don't clone the ethereum-tests repo

--- firefly-setup.sh
+++ firefly-setup.sh
@@ -9,7 +9,14 @@ set -euo pipefail
 
 # install firefly
 cd firefly
-git submodule update --init --recursive
+git submodule update --init deps/evm-semantics
+pushd deps/evm-semantics
+git submodule init
+git submodule deinit tests/ethereum-tests
+git submodule update --recursive
+cd deps/k
+get submodule update --init --recursive
+popd
 pushd deps/evm-semantics/deps/k
 mvn package -U -DskipTests -Dhaskell.backend.skip -Dproject.build.type=FastBuild
 popd
