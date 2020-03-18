13655552eb78ab5d7a51746b421f31e3081ae54c
Guy Repta <50716988+gtrepta@users.noreply.github.com>
2020-03-18 13:47:00 -0500

    jenkins/Jenkinsfile: Clone firefly and run a simple test

--- firefly-run.sh
+++ firefly-run.sh
@@ -4,44 +4,7 @@ set -euo pipefail
 
 export FIREFLY_TOKEN="U052ZEQvZjVpZ0lQbjd1R1NlNFJpZGNTbVppbVFFSng4RWhOclpaTVk2RT0="
 
-# install openzeppelin-contracts
-git clone 'https://github.com/OpenZeppelin/openzeppelin-contracts'
-cd openzeppelin-contracts
-git checkout b8c8308d77beaa733104d1d66ec5f2962df81711
-npm install
-node_modules/.bin/truffle compile
-
-# fetch compiled build data
-zip ../compiled.zip -r build/
-cd ..
-
-# launch kevm server, wait for it to start
-PORT=8545
-cd evm-semantics
-./kevm web3-ganache "$PORT" --shutdownable &
-kevm_client_pid="$!"
-cd ..
-while (! netcat -z 127.0.0.1 "$PORT") ; do sleep 0.1; done
-
-# run some tests through truffle
-cd openzeppelin-contracts
-node_modules/.bin/truffle test test/token/ERC20/ERC20Detailed.test.js &> ../report.txt
-node_modules/.bin/truffle test test/token/ERC20/ERC20Mintable.test.js &>> ../report.txt
-cd ..
-
-# recover coverage data
-cd evm-semantics
-./kevm web3-send "$PORT" 'firefly_getCoverageData' &> ../coverage.json
-cd ..
-
-# close kevm (optional)
-cd evm-semantics
-./kevm web3-send "$PORT" 'firefly_shutdown'
-cd ..
-
-# post the reports
-curl -X POST -F access-token="$FIREFLY_TOKEN"                                        \
-             -F 'status=pass'                                                        \
-             -F 'file=@report.txt'                                                   \
-             -F 'file2=@coverage.json'                                               \
-             -F 'file3=@compiled.zip' 'https://sandbox.fireflyblockchain.com/report'
+cd firefly
+firefly prep tests/tools/random-testing/coverage
+firefly launch tests/tools/random-testing/coverage 8540
+firefly test tests/tools/random-testing/coverage 8540
--- firefly-setup.sh
+++ firefly-setup.sh
@@ -7,13 +7,9 @@ set -euo pipefail
 # export FIREFLY_TOKEN="U052ZEQvZjVpZ0lQbjd1R1NlNFJpZGNTbVppbVFFSng4RWhOclpaTVk2RT0="
 # curl https://sandbox.fireflyblockchain.com/script?firefly_token="$FIREFLY_TOKEN" | sh
 
-curl --location --output k.tar.gz 'https://github.com/kframework/k/releases/download/v5.0.0-280480e/k-nightly.tar.gz'
-tar --verbose --extract --file k.tar.gz
-export K_RELEASE=$(pwd)/k
-
-# install evm-semantics
-git clone https://github.com/kframework/evm-semantics.git
-cd evm-semantics
-git checkout 22569ff809d73903894db5a7b0dad0eb3bc82a73
-git submodule update --init --recursive -- deps/plugin
-make build-web3
+# install firefly
+git clone git@github.com:runtimeverification/firefly.git
+cd firefly
+git submodule update --init --recursive
+make deps
+make build-coverage-web3
