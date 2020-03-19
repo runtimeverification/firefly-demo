78f5e0973b6d08e19a66a885efc8e8671b1420bc
Guy Repta <50716988+gtrepta@users.noreply.github.com>
2020-03-19 13:24:01 -0500

    firefly-run: Export environment variables for firefly script for now

--- firefly-run.sh
+++ firefly-run.sh
@@ -4,7 +4,11 @@ set -euo pipefail
 
 export FIREFLY_TOKEN="U052ZEQvZjVpZ0lQbjd1R1NlNFJpZGNTbVppbVFFSng4RWhOclpaTVk2RT0="
 
-cd firefly
-./firefly prep ../tests/HelloWorld
-./firefly launch ../tests/HelloWorld 8145
-./firefly test ../tests/HelloWorld 8145
+export KEVM_DIR=$(pwd)/firefly/deps/evm-semantics
+export KEVM_DEFN_DIR=$(pwd)/firefly/.build/defn/coverage
+export DEMOS_DIR=$(pwd)/tests
+export PATH=$(pwd)/firefly:$PATH
+
+firefly prep tests/HelloWorld
+firefly launch tests/HelloWorld 8145
+firefly test tests/HelloWorld 8145
