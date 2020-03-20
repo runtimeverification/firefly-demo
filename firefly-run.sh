c7fb8391a91430561454d693f6311ae4e05d7bff
Vladimir Ciobanu <admin@cvlad.info>
2020-03-20 11:49:55 +0200

    add tag and commit

--- firefly-run.sh
+++ firefly-run.sh
@@ -8,16 +8,19 @@ export KEVM_DIR=$(pwd)/firefly/deps/evm-semantics
 export KEVM_DEFN_DIR=$(pwd)/firefly/.build/defn/coverage
 export DEMOS_DIR=$(pwd)/tests
 export PATH=$(pwd)/firefly:$PATH
+export SHA=$(git rev-parse HEAD)
 
 firefly prep tests/HelloWorld
 firefly launch tests/HelloWorld 8145
 firefly test tests/HelloWorld 8145
 
 cd tests/HelloWorld
-curl -X POST -f                                     \
-     -F access-token="$FIREFLY_TOKEN"               \
-     -F 'status=pass'                               \
-     -F 'file=@report.txt'                          \
-     -F 'file2=@coverage_data.zip'                  \
-     -F 'file3=@compiled.zip'                       \
+curl -X POST -f                                                 \
+     -F access-token="$FIREFLY_TOKEN"                           \
+     -F commit="github://runtimeverification/firefly-demo/$SHA" \
+     -F 'status=pass'                                           \
+     -F 'tag="hello world"'                                     \
+     -F 'file=@report.txt'                                      \
+     -F 'file2=@coverage_data.zip'                              \
+     -F 'file3=@compiled.zip'                                   \
      'https://pr-37.sandbox.fireflyblockchain.com/report'
