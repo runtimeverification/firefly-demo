2bd7270e76d1a5f82f5cb57f49a2e975248aab7c
gtrepta <50716988+gtrepta@users.noreply.github.com>
2020-03-24 09:40:56 -0500

    Update end-to-end with Firefly (#17)
    * jenkins/Jenkinsfile: Clone firefly and run a simple test
    
    * firefly-setup: use ssh protocol to clone repo
    
    * Try to use Jenkins to clone Firefly
    
    * firefly-setup: Skip building haskell backend for K
    
    * firefly-run: Add path to firefly command
    
    * firefly-setup.sh: Don't clone the ethereum-tests repo
    
    * jenkins/Jenkinsfile: Formatting and remove GITHUB_TOKEN
    
    * firefly-setup.sh: fix a typo
    
    * firefly-setup: Change submodule update strategy
    
    * tests/HelloWorld: Add simple truffle test
    
    * firefly-run: Export environment variables for firefly script for now
    
    * firefly-run: Upload the coverage report
    
    * firefly-run: Upload the report to PR-37
    
    * add tag and commit
    
    * firefly-run: PR-37 was merged, so upload to main sandbox again
    
    Co-authored-by: Vladimir Ciobanu <admin@cvlad.info>

--- firefly-run.sh
+++ firefly-run.sh
@@ -2,46 +2,25 @@
 
 set -euo pipefail
 
-export FIREFLY_TOKEN="U052ZEQvZjVpZ0lQbjd1R1NlNFJpZGNTbVppbVFFSng4RWhOclpaTVk2RT0="
+export FIREFLY_TOKEN="MCtGM1JoMlpOTXJjRzk4R29GSmR3QUxvbVVVOG9wNW4rM1ZDMnEvK2VBaz0="
 
-# install openzeppelin-contracts
-git clone 'https://github.com/OpenZeppelin/openzeppelin-contracts'
-cd openzeppelin-contracts
-git checkout b8c8308d77beaa733104d1d66ec5f2962df81711
-npm install
-node_modules/.bin/truffle compile
+export KEVM_DIR=$(pwd)/firefly/deps/evm-semantics
+export KEVM_DEFN_DIR=$(pwd)/firefly/.build/defn/coverage
+export DEMOS_DIR=$(pwd)/tests
+export PATH=$(pwd)/firefly:$PATH
+export SHA=$(git rev-parse HEAD)
 
-# fetch compiled build data
-zip ../compiled.zip -r build/
-cd ..
+firefly prep tests/HelloWorld
+firefly launch tests/HelloWorld 8145
+firefly test tests/HelloWorld 8145
 
-# launch kevm server, wait for it to start
-PORT=8545
-cd evm-semantics
-./kevm web3-ganache "$PORT" --shutdownable &
-kevm_client_pid="$!"
-cd ..
-while (! netcat -z 127.0.0.1 "$PORT") ; do sleep 0.1; done
-
-# run some tests through truffle
-cd openzeppelin-contracts
-node_modules/.bin/truffle test test/token/ERC20/ERC20Detailed.test.js &> ../report.txt
-node_modules/.bin/truffle test test/token/ERC20/ERC20Mintable.test.js &>> ../report.txt
-cd ..
-
-# recover coverage data
-cd evm-semantics
-./kevm web3-send "$PORT" 'firefly_getCoverageData' &> ../coverage.json
-cd ..
-
-# close kevm (optional)
-cd evm-semantics
-./kevm web3-send "$PORT" 'firefly_shutdown'
-cd ..
-
-# post the reports
-curl -X POST -F access-token="$FIREFLY_TOKEN"                                        \
-             -F 'status=pass'                                                        \
-             -F 'file=@report.txt'                                                   \
-             -F 'file2=@coverage.json'                                               \
-             -F 'file3=@compiled.zip' 'https://sandbox.fireflyblockchain.com/report'
+cd tests/HelloWorld
+curl -X POST -f                                                 \
+     -F access-token="$FIREFLY_TOKEN"                           \
+     -F commit="github://runtimeverification/firefly-demo/$SHA" \
+     -F 'status=pass'                                           \
+     -F 'tag="hello world"'                                     \
+     -F 'file=@report.txt'                                      \
+     -F 'file2=@coverage_data.zip'                              \
+     -F 'file3=@compiled.zip'                                   \
+     'https://sandbox.fireflyblockchain.com/report'
--- firefly-setup.sh
+++ firefly-setup.sh
@@ -7,13 +7,13 @@ set -euo pipefail
 # export FIREFLY_TOKEN="U052ZEQvZjVpZ0lQbjd1R1NlNFJpZGNTbVppbVFFSng4RWhOclpaTVk2RT0="
 # curl https://sandbox.fireflyblockchain.com/script?firefly_token="$FIREFLY_TOKEN" | sh
 
-curl --location --output k.tar.gz 'https://github.com/kframework/k/releases/download/v5.0.0-280480e/k-nightly.tar.gz'
-tar --verbose --extract --file k.tar.gz
-export K_RELEASE=$(pwd)/k
-
-# install evm-semantics
-git clone https://github.com/kframework/evm-semantics.git
-cd evm-semantics
-git checkout 22569ff809d73903894db5a7b0dad0eb3bc82a73
-git submodule update --init --recursive -- deps/plugin
-make build-web3
+# install firefly
+cd firefly
+git submodule update --init deps/evm-semantics
+pushd deps/evm-semantics
+git submodule update --init --recursive -- deps/
+popd
+pushd deps/evm-semantics/deps/k
+mvn package -U -DskipTests -Dhaskell.backend.skip -Dproject.build.type=FastBuild
+popd
+make build-coverage-web3
