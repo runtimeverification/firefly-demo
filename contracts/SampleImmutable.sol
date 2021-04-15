fb53773c5d0299c8bd442507e8f1b76b0a024f63
Andrei Vacaru <andrei.vacaru@runtimeverification.com>
2021-04-15 13:02:48 +0300

    add more functions

--- contracts/SampleImmutable.sol
+++ contracts/SampleImmutable.sol
@@ -2,6 +2,7 @@ pragma solidity >=0.6.12;
 
 contract SampleImmutable {
     uint256 public immutable imt;
+    mapping (uint256 => uint256) store;
 
     constructor(uint256 value) public {
         imt = value;
@@ -9,5 +10,23 @@ contract SampleImmutable {
 
     function getImmutable() external view returns (uint256 val) {
         val = imt;
+    }
+
+    function sumToImmutable() external view returns (uint256 val) {
+        val = 0;
+        uint256 i = 0;
+        while(i <= imt) {
+            val += i;
+            require(val >= i);
+            i += 1;
+        }
+    }
+
+    function storeAtImmutable(uint256 value) public{
+        store[imt] = value;
+    }
+
+    function readAtImmutable() external view returns (uint256 val) {
+        return store[imt];
     }
 }
